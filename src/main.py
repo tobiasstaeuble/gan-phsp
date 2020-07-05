4b34e535926ef2a35687f1b34f5e2dd70c9612eb
Tobias Stäuble <tobi.staeuble@gmail.com>
2020-07-05 16:57:05 +0200

    corrected env paths

--- src/main.py
+++ src/main.py
@@ -1,8 +1,21 @@
-# Check if imports succeed
-
 import numpy as np
 import torch
 import torch.nn as nn
-from gaga.gaga_helpers import *
+import gaga
 import gatetools.phsp as phsp
+import gatetools as gt
+from torch._C import * # this fails
 
+def infoPHSP(file):
+  return None
+  #gt_phsp_info(file)
+
+
+def loadPHSP(file):
+  return None
+  #gaga.load(file, False, True)
+
+
+#infoPHSP('data/VarianTrueBeam6MV_01.')
+#gt.print_gate_info(True)
+#gt.gt_phsp_info('data/Varian_TrueBeam6MV_01.IAEAphsp')
