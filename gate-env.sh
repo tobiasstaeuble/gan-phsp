93e3cc22b673c82b42772aee4258ed4e53ee2a7c
Tobias Stäuble <tobi.staeuble@gmail.com>
2020-07-06 13:19:37 +0000

    env variables from science cloud machine

--- gate-env.sh
+++ gate-env.sh
@@ -1,6 +1,6 @@
 source install/builds/root/bin/thisroot.sh
 source install/builds/geant4/bin/geant4.sh
-export PATH=$PATH:/mnt/c/Users/mlsostt/Documents/gan-phsp/install/builds/gate-9/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/mnt/c/Users/mlsostt/Documents/gan-phsp/install/libtorch/lib
+export PATH=$PATH:/home/ubuntu/gan-phsp/install/builds/gate-9/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/gan-phsp/install/libtorch/lib
 export PATH=$PATH:/usr/local/cuda-11.0/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.0/lib64
