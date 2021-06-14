cb7a659d6468a31e49076467104ec6a417529318
Janic Weber <janic.weber@gmail.com>
2021-06-14 20:36:07 +0000

    rename gate environment

---
+++ gate_env.sh
@@ -0,0 +1,9 @@
+source ~/gan-phsp/install/builds/root/bin/thisroot.sh
+source ~/gan-phsp/install/builds/geant4/bin/geant4.sh
+export PATH=$PATH:/home/ubuntu/gan-phsp/install/builds/gate-9/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/gan-phsp/install/libtorch/lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/gan-phsp/install/pytorch/build/lib
+#export PATH=$PATH:/usr/local/cuda-11.0/bin
+export PATH=$PATH:/usr/local/cuda-10.1/bin
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.0/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/lib64
