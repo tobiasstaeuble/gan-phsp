332eedb4c055771714f8d4eefbabb44fefeb2acb
Janic Weber <janic.weber@gmail.com>
2021-06-14 14:18:04 +0000

    remove empty or altered files

--- gate-env.sh
+++
@@ -1,8 +0,0 @@
-source install/builds/root/bin/thisroot.sh
-source install/builds/geant4/bin/geant4.sh
-export PATH=$PATH:/home/ubuntu/gan-phsp/install/builds/gate-9/bin
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/gan-phsp/install/libtorch/lib
-#export PATH=$PATH:/usr/local/cuda-11.0/bin
-export PATH=$PATH:/usr/local/cuda-10.1/bin
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.0/lib64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/lib64
